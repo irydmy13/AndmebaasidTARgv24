a42b270d00b17de9399dcf8ae54595eda7fd761b
irydmy13 <kotiukir@gmail.com>
2025-01-30 09:42:29 +0200

    Update AndmebaasidDmytrenko.sql

--- AndmebaasidDmytrenko.sql
+++ AndmebaasidDmytrenko.sql
@@ -12,3 +12,19 @@ SELECT * FROM film;
 
 --tabeli kustutamine
 DROP TABLE film;
+
+--andmete sisestamine tabelisse
+INSERT INTO film(filmNimetus, aasta, eelarveHind)
+VALUES('', 2024, 555555.5);
+
+DELETE FROM film WHERE filmID=5;
+
+
+CREATE TABLE zanr(
+zanrID int PRIMARY KEY identity(1,1),
+zanrNimetus varchar(20) UNIQUE
+)
+
+INSERT INTO zanr(zanrNimetus)
+VALUES ('draama'), ('detektiiv');
+SELECT * FROM zanr;
