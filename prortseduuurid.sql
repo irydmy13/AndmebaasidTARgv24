42dc4ad36bf6bc6108946146b2d42b95e68eeabe
irydmy13 <kotiukir@gmail.com>
2025-02-06 09:29:09 +0200

    Update prortseduuurid.sql

--- prortseduuurid.sql
+++ prortseduuurid.sql
@@ -48,3 +48,21 @@ END;
 --kutse
 EXEC kustutaUudis 3;
 EXEC kustutaUudis @id=3;
+
+UPDATE uudised SET kirjeldus = 'uus kirjeldus'
+WHERE kirjeldus LIKE 'puudub';
+SELECT * FROM uudised;
+
+-- pritseduur mis uuendab andmed tabelis/UPDATE
+CREATE PROCEDURE uuendaKirjeldus
+@uuskirjeldus text
+AS
+BEGIN
+SELECT * FROM uudised;
+UPDATE uudised SET kirjeldus = @uuskirjeldus
+WHERE kirjeldus LIKE 'puudub';
+SELECT * FROM uudised;
+END;
+
+-- kutse
+EXEC uuendaKirjeldus 'uus tekst kirjelduses';
